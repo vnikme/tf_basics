304d73e6a5a52c02caf4570d423de5eb6aea6513
Vadim Nikulin <wad-ya@yandex.ru>
2016-12-17 22:39:33 +0000

    Dumps of current state.

--- skip_gram.py
+++ skip_gram.py
@@ -3,7 +3,7 @@
 
 
 import tensorflow as tf
-import math, random, sys
+import json, math, random, shutil, sys
 
 
 # make lower case
@@ -111,35 +111,85 @@ def print_nearest(embed_weights, inputs, id2word, sess, target, count):
     print " ".join([id2word[t] for t in idx])
 
 
+# class for matching word<->id and storing matrixes
+class TWord2Vec:
+    def __init__(self):
+        self.Word2Id = {}
+        self.Id2Word = []
+
+    def Init(self, embedding_size):
+        vocabulary_size = len(self.Id2Word)
+        self.EmbeddingWeights = tf.Variable(tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+        self.NCEWeights = tf.Variable(tf.truncated_normal([vocabulary_size, embedding_size], stddev=1.0 / math.sqrt(embedding_size)))
+        self.NCEBiases = tf.Variable(tf.zeros([vocabulary_size]))
+
+    def CheckWordListConsistency(self, data):
+        if len(self.Word2Id) != len(data["Word2Id"]) or len(self.Id2Word) != len(data["Id2Word"]):
+            return False
+        for a, b in zip(self.Id2Word, data["Id2Word"]):
+            if a != b.encode("utf-8") or self.Word2Id[a] != data["Word2Id"][b]:
+                return False
+        return True
+
+    def Load(self, path):
+        try:
+            s = open(path, "rt").read()
+        except:
+            return False
+        if len(s) == 0:
+            return False
+        data = json.loads(s)
+        if not self.CheckWordListConsistency(data):
+            return False
+        #self.Word2Id = data["Word2Id"]
+        #self.Id2Word = data["Id2Word"]
+        self.EmbeddingWeights = tf.Variable(data["EmbeddingWeights"])
+        self.NCEWeights = tf.Variable(data["NCEWeights"])
+        self.NCEBiases = tf.Variable(data["NCEBiases"])
+        return True
+
+    def Save(self, path, sess):
+        data = {}
+        data["Word2Id"] = self.Word2Id
+        data["Id2Word"] = self.Id2Word
+        data["EmbeddingWeights"] = sess.run(self.EmbeddingWeights).tolist()
+        data["NCEWeights"] = sess.run(self.NCEWeights).tolist()
+        data["NCEBiases"] = sess.run(self.NCEBiases).tolist()
+        open(path, "wt").write(json.dumps(data))
+
+
 # do all stuff
 def main():
     with tf.device('/cpu:0'):
         # define params
         params = sys.argv[1:]
-        input_path, params = params[:1] + [params[1:]]
-        learning_rate, params = map(float, params[:1]) + [params[1:]]
-        embedding_size, batch_size, num_sampled, context_width, count_of_nearest, epoch, print_freq, params = map(int, params[:7]) + [params[7:]]
+        input_path, dump_path, params = params[:2] + [params[2:]]
+        learning_rate, eps, params = map(float, params[:2]) + [params[2:]]
+        embedding_size, batch_size, num_sampled, context_width, count_of_nearest, print_freq, params = map(int, params[:6]) + [params[6:]]
         words, params = params[:4], params[4:]
         # read data, make indexes word <-> id
-        data, word2id, id2word = read_data(sys.argv[1])
+        w2v = TWord2Vec()
+        data, w2v.Word2Id, w2v.Id2Word = read_data(sys.argv[1])
+        if w2v.Load(dump_path):
+            print "Loaded"
+        else:
+            print "Failed to load from '%s'" % dump_path
+            w2v.Init(embedding_size)
         print_data_stats(data, words)
-        vocabulary_size = len(word2id)
+        vocabulary_size = len(w2v.Word2Id)
         # input and output placeholders
         inputs = tf.placeholder(tf.int32, shape = [None])
         labels = tf.placeholder(tf.int32, shape = [None, 1])
-        # matrix and tensor for 'input->embedding' transform
-        embed_weights = tf.Variable(tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
-        embed_tensor = tf.nn.embedding_lookup(embed_weights, inputs)
-        # matrix and bias for 'embedding->target' transform
-        nce_weights = tf.Variable(tf.truncated_normal([vocabulary_size, embedding_size], stddev=1.0 / math.sqrt(embedding_size)))
-        nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+        # tensor for 'input->embedding' transform
+        embed_tensor = tf.nn.embedding_lookup(w2v.EmbeddingWeights, inputs)
+        # define loss
         loss = tf.reduce_mean(
-                    tf.nn.nce_loss(weights=nce_weights,
-                                   biases=nce_biases,
-                                   labels=labels,
-                                   inputs=embed_tensor,
-                                   num_sampled=num_sampled,
-                                   num_classes=vocabulary_size
+                    tf.nn.nce_loss(weights = w2v.NCEWeights,
+                                   biases = w2v.NCEBiases,
+                                   labels = labels,
+                                   inputs = embed_tensor,
+                                   num_sampled = num_sampled,
+                                   num_classes = vocabulary_size
                                   )
                              )
         #optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
@@ -149,19 +199,25 @@ def main():
         sess = tf.Session()
         sess.run(init)
         # generate all batches
-        all_inputs, all_labels = generate_batch(data, word2id, context_width, 1.0)
+        all_inputs, all_labels = generate_batch(data, w2v.Word2Id, context_width, 1.0)
         all_batches_inputs, all_batches_labels = [], []
         for i in xrange(0, len(all_inputs), batch_size):
             all_batches_inputs.append(all_inputs[i:i+batch_size])
             all_batches_labels.append(all_labels[i:i+batch_size])
-        for e in xrange(epoch):
+        epoch = 0
+        while True:
             loss_val = 0.0
             for batch_inputs, batch_labels in zip(all_batches_inputs, all_batches_labels):
                 _, l = sess.run([optimizer, loss], feed_dict = {inputs: batch_inputs, labels: batch_labels})
                 loss_val += (l / len(all_batches_inputs))
                 #print l, len(all_batches_inputs)
-            if e % print_freq == 0 or e + 1 == epoch:
-                pred = sess.run([embed_tensor], feed_dict = {inputs: [word2id[t] for t in words]})
+            if epoch % print_freq == 0 or loss_val < eps:
+                try:
+                    shutil.copy(dump_path, dump_path + ".bak")
+                except:
+                    pass
+                w2v.Save(dump_path, sess)
+                pred = sess.run([embed_tensor], feed_dict = {inputs: [w2v.Word2Id[t] for t in words]})
                 a = [float(t) for t in pred[0][0] - pred[0][1]]
                 b = [float(t) for t in pred[0][2] - pred[0][3]]
                 c = [float(t) for t in pred[0][0] - pred[0][1] - pred[0][2] + pred[0][3]]
@@ -173,10 +229,12 @@ def main():
                 a = [t / a_abs for t in a]
                 b = [t / b_abs for t in b]
                 print "%.2f\t%.4f\t%.4f\t%.4f\t%.4f" % (loss_val, sum([i * j for i, j in zip(a, b)]), a_abs, b_abs, c_abs)
-                print_analogy(pred[0][0], pred[0][1], pred[0][2], inputs, embed_weights, id2word, sess, count_of_nearest)
-                print_nearest(embed_weights, inputs, id2word, sess, d, count_of_nearest)
-                print_nearest(embed_weights, inputs, id2word, sess, e, count_of_nearest)
+                print_analogy(pred[0][0], pred[0][1], pred[0][2], inputs, w2v.EmbeddingWeights, w2v.Id2Word, sess, count_of_nearest)
+                print_nearest(w2v.EmbeddingWeights, inputs, w2v.Id2Word, sess, d, count_of_nearest)
+                print_nearest(w2v.EmbeddingWeights, inputs, w2v.Id2Word, sess, e, count_of_nearest)
                 print
+            if loss_val < eps:
+                break
 
 
 # entry point
