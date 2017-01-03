1e10ea404f592435ac462ec4c0317cc96d825474
Vadim A. Nikulin <accs@vnik.me>
2017-01-03 17:22:40 +0000

    Train on small series, test on much bigger ones.

--- rnn_sum.py
+++ rnn_sum.py
@@ -24,26 +24,54 @@ def generate_batch(batch_size, bits):
 
 # do all stuff
 def main():
-    max_time, input_size, output_size, state_size = 1000, 2, 1, 5
+    # define params
+    max_time, max_valid_time, batch_size, valid_batch_size, input_size, output_size, state_size, eps = 100, 100000, 1000, 1000, 2, 1, 5, 0.001
     gru = tf.nn.rnn_cell.GRUCell(state_size)
-    x = tf.placeholder(tf.float32, [None, max_time, input_size])
-    output, state = tf.nn.dynamic_rnn(gru, x, dtype = tf.float32)
-    y = tf.placeholder(tf.float32, [None, max_time, output_size])
     w = tf.Variable(tf.random_normal([state_size, output_size]))
     b = tf.Variable(tf.random_normal([output_size]))
-    output = tf.reshape(output, [-1, state_size])
-    output = tf.sigmoid(tf.add(tf.matmul(output, w), b))
-    output = tf.reshape(output, [-1, max_time, output_size])
-    loss = tf.nn.l2_loss(tf.subtract(output, y))
-    optimizer = tf.train.AdamOptimizer(learning_rate = 0.1).minimize(loss)
+    # create learning graph
+    with tf.variable_scope('train'):
+        x = tf.placeholder(tf.float32, [None, max_time, input_size])
+        output, state = tf.nn.dynamic_rnn(gru, x, dtype = tf.float32)
+        y = tf.placeholder(tf.float32, [None, max_time, output_size])
+        output = tf.reshape(output, [-1, state_size])
+        output = tf.sigmoid(tf.add(tf.matmul(output, w), b))
+        output = tf.reshape(output, [-1, max_time, output_size])
+        # define loss and optimizer
+        loss = tf.nn.l2_loss(tf.subtract(output, y))
+        optimizer = tf.train.AdamOptimizer(learning_rate = 0.1).minimize(loss)
+    # define validation and test data and operations
+    with tf.variable_scope('train', reuse = True):
+        valid_x = tf.placeholder(tf.float32, [None, max_valid_time, input_size])
+        output, state = tf.nn.dynamic_rnn(gru, valid_x, dtype = tf.float32)
+        valid_y = tf.placeholder(tf.float32, [None, max_valid_time, output_size])
+        output = tf.reshape(output, [-1, state_size])
+        output = tf.sigmoid(tf.add(tf.matmul(output, w), b))
+        output = tf.reshape(output, [-1, max_valid_time, output_size])
+        valid_loss = tf.nn.l2_loss(tf.subtract(output, valid_y))
+    # begin training
     init = tf.global_variables_initializer()
     sess = tf.Session()
     sess.run(init)
+    cnt = 0
+    valid_batch_x, valid_batch_y = generate_batch(valid_batch_size, max_valid_time)
     while True:
-        batch_x, batch_y = generate_batch(10000, max_time)
+        batch_x, batch_y = generate_batch(batch_size, max_time)
         res, l = sess.run([optimizer, loss], feed_dict = {x: batch_x, y: batch_y})
-        print l
-    return
+        l /= (batch_size * max_time)
+        if cnt % 10 == 0:
+            vl = sess.run(valid_loss, feed_dict = {valid_x: valid_batch_x, valid_y: valid_batch_y})
+            vl /= (valid_batch_size * max_valid_time)
+            print l, vl
+        else:
+            print l
+        if vl < eps:
+            break
+        cnt += 1
+    test_x, test_y = generate_batch(valid_batch_size, max_valid_time)
+    tl = sess.run(valid_loss, feed_dict = {valid_x: test_x, valid_y: test_y})
+    tl /= (valid_batch_size * max_valid_time)
+    print tl
 
 
 # entry point
