00bae5fed143e0eabc55b3ae2e3a84786316895c
Vadim A. Nikulin <accs@vnik.me>
2017-01-10 00:39:11 +0000

    Proper loss function. Train and apply modes. Still a lot of junk :(

--- char_rnn.py
+++ char_rnn.py
@@ -28,10 +28,10 @@ def iterate_symbols(path):
     c = 0
     for line in open(path):
         c += 1
-        if c % 1000 == 0:
+        if c % 1000000 == 0:
             print c / 1000000
             sys.stdout.flush()
-            if c / 1000 >= 1:
+            if c / 1000000 >= 30:
                 break
         #line = to_wide_lower(line)
         line = line.decode("utf-8")
@@ -134,8 +134,11 @@ def test():
 # do all stuff
 def main():
     # define params
-    max_time, batch_size, state_size, learning_rate = 25, 1000, 200, 0.001
+    max_time, batch_size, state_size, learning_rate = 16, 5000, 1024, 0.001
+    #max_time, batch_size, state_size, learning_rate = 16, 1, 512, 0.001
+    #learning_rate /= batch_size
     path = "data/all"
+    #path = "char_rnn.py"
     vocabulary_size = len(all_syms)
 
     # read and convert data
@@ -151,37 +154,52 @@ def main():
     # create learning graph
     state_x = tf.placeholder(tf.float32, [None, state_size])
     with tf.variable_scope('train'):
-        output, state = tf.nn.dynamic_rnn(gru, tf.one_hot(x, vocabulary_size, on_value = 1.0), initial_state = state_x, sequence_length = lengths, dtype = tf.float32)
+        output, state = tf.nn.dynamic_rnn(gru, tf.one_hot(x, vocabulary_size, on_value = 1.0), initial_state = state_x, sequence_length = lengths, dtype = tf.float32, swap_memory = True)
     output = tf.reshape(output, [-1, state_size])
     output = tf.add(tf.matmul(output, w), b)
     output = tf.reshape(output, [-1, max_time, vocabulary_size])
-    output = tf.nn.softmax(output)
+    #output = tf.nn.softmax(output)
     y = tf.placeholder(tf.int32, [None, max_time])
 
     # define loss and optimizer
     ohy = tf.one_hot(y, vocabulary_size, on_value = 1.0)
-    loss = -tf.reduce_sum(tf.mul(output, ohy))
+    loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(output, ohy))
     optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(loss)
 
     # create output tensors for applying
     apply_x = tf.placeholder(tf.int32, [None, max_time])                    # we will apply it sym-by-sym
     apply_state_x = tf.placeholder(tf.float32, [None, state_size])
     with tf.variable_scope('train', reuse = True):
-        apply_output, apply_state = tf.nn.dynamic_rnn(gru, tf.one_hot(apply_x, vocabulary_size, on_value = 1.0), initial_state = apply_state_x, sequence_length = lengths, dtype = tf.float32)
+        apply_output, apply_state = tf.nn.dynamic_rnn(gru, tf.one_hot(apply_x, vocabulary_size, on_value = 1.0), initial_state = apply_state_x, sequence_length = lengths, dtype = tf.float32, swap_memory = True)
     apply_output = tf.reshape(apply_output, [-1, state_size])
     apply_output = tf.add(tf.matmul(apply_output, w), b)
     apply_output = tf.reshape(apply_output, [-1, max_time, vocabulary_size])
     apply_output = tf.nn.softmax(apply_output)
 
-    # begin training
+    # create saver
+    saver = tf.train.Saver(max_to_keep = 20)
+
+    # prepare variables
     init = tf.global_variables_initializer()
     sess = tf.Session()
     sess.run(init)
     cnt = 0
     steps = (len(data) - 1) / (batch_size * max_time)
+    zero_state = sess.run(gru.zero_state(batch_size, tf.float32))
+    apply_zero_state = sess.run(gru.zero_state(1, tf.float32))
+    prev_loss = 0.0
+
+    # apply mode
+    if len(sys.argv) > 1:
+        saver.restore(sess, sys.argv[1])
+        while True:
+            seed = raw_input("Enter phrase: ")
+            print make_sample1(sess, apply_x, apply_state_x, apply_output, apply_state, lengths, apply_zero_state, int(sys.argv[2]), seed, max_time)
+
+    # training mode
     while True:
         l, c, i = 0.0, 0, 0
-        cur_state = sess.run(gru.zero_state(batch_size, tf.float32))
+        cur_state = zero_state
         for i in xrange(steps):
             #print (i + 1) * max_time + steps * max_time * (batch_size - 1) + 1, len(data), steps
             batch_x, batch_y = [], []
@@ -192,26 +210,27 @@ def main():
             #print batch_x
             #print batch_y
             #print
-            if cnt < 10:
-                cur_state = sess.run(gru.zero_state(batch_size, tf.float32))
-            _, cur_state, _l, dump = sess.run([optimizer, state, loss, tf.mul(output, ohy)], feed_dict = {x: batch_x, y: batch_y, state_x: cur_state, lengths: [max_time] * batch_size})
-            _l /= (batch_size * max_time)
+            if c == 0 or l / c > 0.03:
+                cur_state = zero_state
+            _, cur_state, _l = sess.run([optimizer, state, loss], feed_dict = {x: batch_x, y: batch_y, state_x: cur_state, lengths: [max_time] * batch_size})
+            #_l /= (batch_size * max_time)
             l += _l
             c += 1
             #print_matrs(dump)
-            print "Progress: %.1f%%, loss: %f" % (i * 100.0 / steps, -_l)
+            print "Progress: %.1f%%, loss: %f" % (i * 100.0 / steps, _l)
             sys.stdout.flush()
-        while True:
-            seed = raw_input("Enter begining of string: ") if (-l / c >= 0.3) else "Посадил дед ре"
-            if seed == "exit":
-                break
-            print make_sample1(sess, apply_x, apply_state_x, apply_output, apply_state, lengths, sess.run(gru.zero_state(1, tf.float32)), 100, seed, max_time)
-            #print make_sample1(sess, x, state_x, output, state, lengths, sess.run(gru.zero_state(1, tf.float32)), 1000, seed, max_time)
-            if -l / c < 0.3:
-                break
-        print "Loss: %.5f\n" % (-l / c)
+            #if _l < 0.3:
+            #    break
+        seed = "Посадил дед ре"
+        #seed = "#!/"
+        print make_sample1(sess, apply_x, apply_state_x, apply_output, apply_state, lengths, apply_zero_state, 1000, seed, max_time)
+        print "Loss: %.5f\tdiff with prev: %.5f\tlrate: %.5f\n" % (l / c, l / c - prev_loss, learning_rate)
         sys.stdout.flush()
+        saver.save(sess, "dumps/dump", global_step = cnt)
         cnt += 1
+        #if -l / c - prev_loss < 0.00001:
+        #    learning_rate *= 0.95
+        prev_loss = l / c
 
 
 # entry point
