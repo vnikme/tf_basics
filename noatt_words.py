f946e073a1266362e1b688882498e2963730d403
Vadim A. Nikulin <accs@vnik.me>
2017-02-01 20:07:00 +0000

    Intermediate version. Todo: replace words.py with this more simple model.

--- noatt_words.py
+++ noatt_words.py
@@ -46,7 +46,7 @@ def iterate_chars(path, mask):
             yield ch
 
 
-def iterate_words(char_iterator):
+def _iterate_words(char_iterator):
     is_space, word = True, ""
     for ch in char_iterator:
         if ch in WORDBRK:
@@ -69,8 +69,18 @@ def iterate_words(char_iterator):
         yield word
 
 
-def word_to_codes(word):
-    return [ALL_SYMS.index(ch) for ch in word]
+def iterate_words(char_iterator, word_len):
+    for word in _iterate_words(char_iterator):
+        for i in xrange(0, len(word), word_len):
+            yield word[i : i + word_len]
+
+
+def word_to_codes(word, word_len):
+    word = [ALL_SYMS.index(ch) for ch in word]
+    k = word_len - len(word)
+    if k > 0:
+        word += ([STOP] * k)
+    return word
 
 
 class TWord:
@@ -83,12 +93,12 @@ class TWord:
         self.dword = word[0 : l - 1]        # input to decoder
 
 
-def read_words():
+def read_words(word_len):
     data = {}
-    for src_word in iterate_words(iterate_chars("lib_ru/public_html/book", "*.txt")):
-    #for src_word in iterate_words(iterate_chars("data", "all")):
+    for src_word in iterate_words(iterate_chars("lib_ru/public_html/book", "*.txt"), word_len - 1):
+    #for src_word in iterate_words(iterate_chars("data", "all"), word_len - 1):
         if src_word not in data:
-            word = word_to_codes(src_word)
+            word = word_to_codes(src_word, word_len - 1)
             data[src_word] = TWord(word)
         else:
             data[src_word].count += 1
@@ -97,30 +107,17 @@ def read_words():
     return data
 
 
-class TBatch:
-    def __init__(self):
-        self.x, self.dx, self.m = [], [], []
-
-
-def iterate_batches(data, batch_size, long_word_limit, long_batch_size):
+def iterate_batches(data, batch_size):
     words = data.keys()
-    lens = {}
+    random.shuffle(words)
+    x, dx, m = [], [], []
     for txt in words:
         word = data[txt]
-        l = len(word.word)
-        if l not in lens:
-            lens[l] = TBatch()
-        b = lens[l]
-        b.x.append(word.word)
-        b.dx.append(word.dword)
-        #b.m.append(math.sqrt(word.count))
-        b.m.append(math.log(word.count + 1.0))
-    word_lens = lens.keys()
-    random.shuffle(word_lens)
-    for word_len in word_lens:
-        b = lens[word_len]
-        for i in xrange(0, len(b.x), batch_size if word_len <= long_word_limit else long_batch_size):
-            yield word_len, b.x[i : i + batch_size], b.dx[i : i + batch_size], b.m[i : i + batch_size]
+        x.append(word.word)
+        dx.append(word.dword)
+        m.append(math.log(word.count + 1.0))
+    for i in xrange(0, len(x), batch_size):
+        yield x[i : i + batch_size], dx[i : i + batch_size], m[i : i + batch_size]
 
 
 # input shape: batch*time*input_state
@@ -150,21 +147,27 @@ def choose_random(distr):
 
 
 class TWordPackager:
-    def __init__(self, state_size, vocabulary_size):
+    def __init__(self, embedding_size, word_len, state_size, vocabulary_size):
+        self.embedding_size = embedding_size
+        self.word_len = word_len
         self.state_size = state_size
         self.vocabulary_size = vocabulary_size
         # create encoder variables
         with tf.variable_scope('encoder'):
-            self.encoder_cell = tf.nn.rnn_cell.GRUCell(self.state_size)
-            self.encoder_input_placeholders, self.encoder_outputs = {}, {}
-            self.encoder_input_placeholders[1], self.encoder_outputs[1] = self._create_encoder(1)
+            self.encoder_input = tf.placeholder(tf.int32, [None, self.word_len])
+            self.embedding_weights = tf.Variable(tf.random_uniform([self.vocabulary_size, self.embedding_size], -0.01, 0.01), name = "emb")
+            self.encoder_w = tf.Variable(tf.random_normal([self.word_len * self.embedding_size, self.state_size], -0.01, 0.01), name = "ew")
+            self.encoder_b = tf.Variable(tf.random_normal([self.state_size], -0.01, 0.01), name = "eb")
+            embed_tensor = tf.nn.embedding_lookup(self.embedding_weights, self.encoder_input)
+            self.encoder_output = attention(embed_tensor, self.word_len, self.embedding_size, self.state_size, self.encoder_w, self.encoder_b)
         # create decoder variables
         with tf.variable_scope('decoder'):
+            self.decoder_input = tf.placeholder(tf.int32, [None, self.word_len])
             self.decoder_cell = tf.nn.rnn_cell.GRUCell(self.state_size)
-            self.decoder_input_placeholders, self.decoder_outputs = {}, {}
-            self.decoder_w = tf.Variable(tf.random_normal([self.state_size, self.vocabulary_size]), name = "w")
-            self.decoder_b = tf.Variable(tf.random_normal([self.vocabulary_size]), name = "b")
-            self.decoder_input_placeholders[1], self.decoder_outputs[1] = self._create_decoder(1, self.encoder_outputs[1])
+            self.decoder_w = tf.Variable(tf.random_normal([self.state_size, self.vocabulary_size], -0.01, 0.01), name = "dw")
+            self.decoder_b = tf.Variable(tf.random_normal([self.vocabulary_size], -0.01, 0.01), name = "db")
+            output, state = tf.nn.dynamic_rnn(self.decoder_cell, tf.one_hot(self.decoder_input, self.vocabulary_size, on_value = 1.0), initial_state = self.encoder_output, dtype = tf.float32)
+            self.decoder_output = projection(output, self.state_size, self.word_len, self.vocabulary_size, self.decoder_w, self.decoder_b)
         # create decoder variables for applying
         with tf.variable_scope('decoder', reuse = True):
             self.apply_decoder_input_placeholder = tf.placeholder(tf.int32, [None, 1])
@@ -173,32 +176,6 @@ class TWordPackager:
             output = projection(output, self.state_size, 1, self.vocabulary_size, self.decoder_w, self.decoder_b)
             # renorm output logits for sampling
             self.apply_decoder_output = tf.nn.softmax(output)
-
-    def _create_encoder(self, word_len):
-        placeholder = tf.placeholder(tf.int32, [None, word_len])
-        output, state = tf.nn.dynamic_rnn(self.encoder_cell, tf.one_hot(placeholder, self.vocabulary_size, on_value = 1.0), dtype = tf.float32)
-        return placeholder, state
-
-    def _create_decoder(self, word_len, encoder_output):
-        placeholder = tf.placeholder(tf.int32, [None, word_len])
-        output, state = tf.nn.dynamic_rnn(self.decoder_cell, tf.one_hot(placeholder, self.vocabulary_size, on_value = 1.0), initial_state = encoder_output, dtype = tf.float32)
-        output = projection(output, self.state_size, word_len, self.vocabulary_size, self.decoder_w, self.decoder_b)
-        return placeholder, output
-
-    def full_encoder(self, word_len):
-        if word_len not in self.encoder_input_placeholders:
-            with tf.variable_scope('encoder', reuse = True):
-                self.encoder_input_placeholders[word_len], self.encoder_outputs[word_len] = self._create_encoder(word_len)
-        encoder_input, encoder_output = self.encoder_input_placeholders[word_len], self.encoder_outputs[word_len]
-        return encoder_input, encoder_output
-
-    def full_encoder_decoder(self, word_len):
-        encoder_input, encoder_output = self.full_encoder(word_len)
-        if word_len not in self.decoder_input_placeholders:
-            with tf.variable_scope('encoder', reuse = True):
-                self.decoder_input_placeholders[word_len], self.decoder_outputs[word_len] = self._create_decoder(word_len, encoder_output)
-        decoder_input, decoder_output = self.decoder_input_placeholders[word_len], self.decoder_outputs[word_len]
-        return encoder_input, decoder_input, decoder_output
 
     def to_json(self, sess):
         m = {}
@@ -219,9 +196,8 @@ class TWordPackager:
         sess.run(ops)
 
     def encode_word(self, sess, word):
-        word = TWord(word_to_codes(word))
-        x, op = self.full_encoder(len(word.word))
-        return sess.run(op, feed_dict = {x: [word.word]})[0]
+        word = TWord(word_to_codes(word, self.word_len - 1))
+        return sess.run(self.encoder_output, feed_dict = {self.encoder_input: [word.word]})[0]
 
     def _decode_word(self, sess, word, limit_word_len, choose_func):
         cur_state = word
@@ -245,13 +221,13 @@ class TWordPackager:
         return self._decode_word(sess, word, limit_word_len, lambda probs: np.argmax(probs))
 
 
-def some_fixed_text():
+def some_fixed_text(word_len):
     text = "При этом экс-губернатор крайне разозлился тем, что в Интернете активно распространяются фотографии, где он выглядит, как бомж, прибывший на знаковое политическое событие в Соединенные Штаты. Так, на одном из снимков видно, что Саакашвили в непрезентабельной одежде стоит в зале аэропорта Нью-Йорка. Создается впечатление, что утром он был сильно пьян и поэтому надел на себя первое, что попалось ему под руку. На другом снимке Саакашвили одиноко стоит в кустах с телефоном в руке в то время, когда основная публика находится на инаугурации Трампа."
-    return iterate_words(text.decode("utf-8"))
+    return iterate_words(text.decode("utf-8"), word_len - 1)
 
 
-def iterate_keyboard_input():
-    return iterate_words(raw_input().decode("utf-8"))
+def iterate_keyboard_input(word_len):
+    return iterate_words(raw_input().decode("utf-8"), word_len - 1)
 
 
 def sample_words(wp, sess, limit_word_len, word_iterator):
@@ -264,104 +240,69 @@ def sample_words(wp, sess, limit_word_len, word_iterator):
     return original, predicted_max, predicted_rand
 
 
-class TOptimizerSelector:
-    def __init__(self, wp, learning_rate):
-        self.wp = wp
-        self.learning_rate = learning_rate
-        self.mults = tf.placeholder(tf.float32, [None])
-        self.target_losses, self.optimizers = {}, {}
-
-    def choose(self, word_len):
-        encoder_input, decoder_input, decoder_output = self.wp.full_encoder_decoder(word_len)
-        if word_len not in self.optimizers:
-            ohy = tf.one_hot(encoder_input, self.wp.vocabulary_size, on_value = 1.0)
-            loss = tf.nn.sigmoid_cross_entropy_with_logits(decoder_output, ohy)
-            loss = tf.reduce_mean(loss, 2)
-            loss = tf.reduce_mean(loss, 1)
-            loss = tf.mul(loss, self.mults)
-            loss = tf.reduce_mean(loss)
-            self.target_losses[word_len] = loss
-            #optimizer = tf.train.GradientDescentOptimizer(learning_rate = self.learning_rate)
-            optimizer = tf.train.GradientDescentOptimizer(learning_rate = self.learning_rate).minimize(loss)
-            #optimizer = tf.train.AdamOptimizer(learning_rate = self.learning_rate).minimize(loss, colocate_gradients_with_ops = True)
-            #optimizer = tf.train.AdamOptimizer(learning_rate = self.learning_rate).minimize(loss)
-            self.optimizers[word_len] = optimizer
-        return encoder_input, decoder_input, self.mults, self.target_losses[word_len], self.optimizers[word_len]
-
-
 def correct_learning_rate_multiplier(losses):
-    inc, dec = 0, 0
-    losses = losses[-10:]
-    for i in xrange(1, len(losses)):
-        if losses[i - 1] < losses[i]:
-            inc += 1
-        else:
-            dec += 1
-    #print inc, dec
-    if inc == 0 and dec > 8:
-        return 1.05
-    elif inc >= 2:
-        return 0.95
+    if len(losses) >= 2 and losses[-2] * 2 < losses[-1]:
+        return 0.7
     return 1.0
 
 
 def main():
     # define params
-    batch_size, long_word_limit, max_word_len, long_batch_size, state_size, limit_word_len = 5000, 20, 30, 100, 1024, 50
-    learning_rate = tf.Variable(0.1, trainable=False)
+    batch_size, max_word_len, embedding_size, state_size, limit_word_len = 10000, 25, 5, 64, 50
+    learning_rate = tf.Variable(0.001, trainable = False)
 
-    wp = TWordPackager(state_size, VOCABULARY_SIZE)
-    opt = TOptimizerSelector(wp, learning_rate)
+    wp = TWordPackager(embedding_size, max_word_len, state_size, VOCABULARY_SIZE)
 
     # prepare variables
     sess = tf.Session()
 
     # read all data
-    data = read_words()
+    data = read_words(max_word_len)
 
-    # retrive all optimizers
-    for word_len, batch_x, batch_dx, batch_m in iterate_batches(data, len(data), long_word_limit, len(data)):
-        print word_len, len(batch_x)
-    #    x, dx, y, mults, loss, optimizer = opt.choose(word_len)
+    # create loss and optimizer
+    mults = tf.placeholder(tf.float32, [None])
+    ohy = tf.one_hot(wp.encoder_input, wp.vocabulary_size, on_value = 1.0)
+    loss = tf.nn.sigmoid_cross_entropy_with_logits(wp.decoder_output, ohy)
+    loss = tf.reduce_mean(loss, 2)
+    loss = tf.reduce_mean(loss, 1)
+    loss = tf.mul(loss, mults)
+    loss = tf.reduce_mean(loss)
+    #optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(loss)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate = learning_rate).minimize(loss)
 
     # initialize global variables
     init = tf.global_variables_initializer()
     sess.run(init)
 
-    wp.to_json(sess)
-
-    if False:
+    if True:
         wp.from_json(open("dump.char", "rt").read(), sess)
-        print sample_words(wp, sess, max_word_len, some_fixed_text(max_word_len))[1]
+        original, predicted_max, predicted_rand = sample_words(wp, sess, limit_word_len, some_fixed_text(max_word_len))
+        print original
+        print predicted_max
+        print predicted_rand
+        print
         sys.stdout.flush()
     elif False:
         wp.from_json(open("dump.char", "rt").read(), sess)
         while True:
             print sample_words(wp, sess, max_word_len, iterate_keyboard_input(max_word_len))[1]
 
-    epoch, learning_rate_multiplier = 0, 1.0
+    epoch = 0
     all_batches = []
-    for word_len, batch_x, batch_dx, batch_m in iterate_batches(data, batch_size, long_word_limit, long_batch_size):
-        if word_len <= max_word_len:
-            all_batches.append([word_len, batch_x, batch_dx, batch_m])
+    for batch_x, batch_dx, batch_m in iterate_batches(data, batch_size):
+        all_batches.append([batch_x, batch_dx, batch_m])
     losses = []
     while True:
         cnt, l = 1e-38, 0.0
-        for word_len, batch_x, batch_dx, batch_m in all_batches:
+        for batch_x, batch_dx, batch_m in all_batches:
             cnt += 1
-            x, dx, mults, loss, optimizer = opt.choose(word_len)
-            #grad = []
-            #for gv in optimizer.compute_gradients(loss):
-            #    if gv[0] is not None:
-            #        grad.append((gv[0] * learning_rate_multiplier, gv[1]))
-            _, _l = sess.run([optimizer, loss], feed_dict = {x: batch_x, dx: batch_dx, mults: batch_m})
+            _, _l = sess.run([optimizer, loss], feed_dict = {wp.encoder_input: batch_x, wp.decoder_input: batch_dx, mults: batch_m})
             l += _l
         losses.append(l / cnt)
-        learning_rate_multiplier = correct_learning_rate_multiplier(losses)
-        learning_rate *= learning_rate_multiplier
+        learning_rate *= correct_learning_rate_multiplier(losses)
         print "loss: %f\tlearning rate: %.6f\tepoch: %d" % (l / cnt, sess.run(learning_rate), epoch)
-        if epoch % 30 == 0:
-            original, predicted_max, predicted_rand = sample_words(wp, sess, limit_word_len, some_fixed_text())
+        if epoch % 10 == 0:
+            original, predicted_max, predicted_rand = sample_words(wp, sess, limit_word_len, some_fixed_text(max_word_len))
             print original
             print predicted_max
             print predicted_rand
