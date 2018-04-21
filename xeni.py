624e90e51d7f3e570fec899f1e1eec566e27637d
Vadim A. Nikulin <accs@vnik.me>
2018-04-21 11:15:48 +0100

    Add character-level loss for generator (doesn't work).

--- xeni.py
+++ xeni.py
@@ -15,6 +15,8 @@ def read_data(path, max_length):
         if "correctedText" not in js:
             continue
         data.append(js["correctedText"])
+        #if len(data) > 20:
+        #    break
     data = filter(lambda x: len(x) <= max_length - 1, data)
     n = len(data)
     chars = ["stop"] + list(set(''.join(data)))
@@ -60,13 +62,13 @@ def sample1(sess, generator_outputs, generator_input, generator_c_states, genera
 
 def main():
     with tf.Session() as sess:
-        max_time = 64
+        max_time = 32
         data, vocabulary, codes = read_data(sys.argv[1], max_time)
-        batch_size, hidden_size, number_of_layers, vocabulary_size = 8, 256, 2, len(vocabulary)
+        batch_size, hidden_size, number_of_layers, vocabulary_size = 8, 256, 1, len(vocabulary)
 
         with tf.device('/gpu:0'):
             def lstm_cell(k, prefix):
-                return tf.nn.rnn_cell.BasicLSTMCell(hidden_size, name = prefix + str(k))
+                return tf.nn.rnn_cell.LSTMCell(hidden_size, name = prefix + str(k))
 
             generator_input = tf.placeholder(tf.float64, (None, hidden_size))
             generator_c_states, generator_h_states, generator_state_input = [], [], []
@@ -75,7 +77,8 @@ def main():
                 generator_h_states.append(tf.placeholder(tf.float64, [None, hidden_size]))
                 generator_state_input.append(tf.nn.rnn_cell.LSTMStateTuple(c = generator_c_states[-1], h = generator_h_states[-1]))
             generator_state_input = tuple(generator_state_input)
-            generator_cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell(i, "generator_") for i in range(number_of_layers)])
+            generator_base_cells = [lstm_cell(i, "generator_") for i in range(number_of_layers)]
+            generator_cell = tf.nn.rnn_cell.MultiRNNCell(generator_base_cells)
             output, state = generator_input, generator_state_input
             generator_w = tf.get_variable("generator_w", (hidden_size, vocabulary_size), initializer = tf.random_normal_initializer(0.0, 1.0), dtype = tf.float64)
             generator_b = tf.get_variable("generator_b", (vocabulary_size), initializer = tf.random_normal_initializer(0.0, 1.0), dtype = tf.float64)
@@ -89,7 +92,8 @@ def main():
 
             discriminator_input = tf.placeholder(tf.int32, (batch_size, max_time))
             real_input = tf.transpose(tf.one_hot(discriminator_input, vocabulary_size, tf.to_double(1.0), dtype = tf.float64, axis = -1), [1, 0, 2])
-            discriminator_cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell(i, "discriminator_") for i in range(number_of_layers)])
+            discriminator_base_cells = [lstm_cell(i, "discriminator_") for i in range(number_of_layers)]
+            discriminator_cell = tf.nn.rnn_cell.MultiRNNCell(discriminator_base_cells)
             discriminator_w1 = tf.get_variable("discriminator_w1", (vocabulary_size, hidden_size), initializer = tf.random_normal_initializer(0.0, 1.0), dtype = tf.float64)
             discriminator_b1 = tf.get_variable("discriminator_b1", (hidden_size), initializer = tf.random_normal_initializer(0.0, 1.0), dtype = tf.float64)
             discriminator_w2 = tf.get_variable("discriminator_w2", (number_of_layers * hidden_size, 1), initializer = tf.random_normal_initializer(0.0, 1.0), dtype = tf.float64)
@@ -100,6 +104,9 @@ def main():
                 _, discriminator_real_state = discriminator_cell(tf.add(tf.matmul(real_input[i], discriminator_w1), discriminator_b1), discriminator_real_state)
                 _, discriminator_fake_state = discriminator_cell(tf.add(tf.matmul(generator_outputs[i], discriminator_w1), discriminator_b1), discriminator_fake_state)
             discriminator_vars = discriminator_cell.trainable_variables + [discriminator_w1, discriminator_b1, discriminator_w2, discriminator_b2]
+            #for var in tf.trainable_variables():
+            #    print var.name
+            #exit(1)
 
             def reshape_discriminator_state(state):
                 result = [state[i][0] for i in xrange(number_of_layers)]
@@ -111,17 +118,19 @@ def main():
             discriminator_real_state = reshape_discriminator_state(discriminator_real_state)
             discriminator_fake_state = reshape_discriminator_state(discriminator_fake_state)
 
+            char_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = generator_outputs, labels = tf.one_hot(tf.transpose(discriminator_input, [1, 0]), vocabulary_size, tf.to_double(1.0), dtype = tf.float64, axis = -1)))
+
             real_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = discriminator_real_state, labels = tf.ones_like(discriminator_real_state)))
             fake_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = discriminator_fake_state, labels = tf.zeros_like(discriminator_fake_state)))
-            generator_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = discriminator_fake_state, labels = tf.ones_like(discriminator_fake_state)))
+            generator_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = discriminator_fake_state, labels = tf.ones_like(discriminator_fake_state))) + char_loss
             discriminator_loss = real_loss + fake_loss
 
-            #generator_optimizer = tf.train.AdamOptimizer(0.0001).minimize(generator_loss, var_list = generator_vars)
-            #discriminator_optimizer = tf.train.AdamOptimizer(0.0001).minimize(discriminator_loss, var_list = discriminator_vars)
+            generator_optimizer = tf.train.AdamOptimizer(0.0001).minimize(generator_loss, var_list = generator_vars)
+            discriminator_optimizer = tf.train.AdamOptimizer(0.0001).minimize(discriminator_loss, var_list = discriminator_vars)
             #generator_optimizer = tf.train.GradientDescentOptimizer(0.001).minimize(generator_loss, var_list = generator_vars)
             #discriminator_optimizer = tf.train.GradientDescentOptimizer(0.001).minimize(discriminator_loss, var_list = discriminator_vars)
-            generator_optimizer = tf.train.AdadeltaOptimizer(0.001).minimize(generator_loss, var_list = generator_vars)
-            discriminator_optimizer = tf.train.AdadeltaOptimizer(0.001).minimize(discriminator_loss, var_list = discriminator_vars)
+            #generator_optimizer = tf.train.AdadeltaOptimizer(0.001).minimize(generator_loss, var_list = generator_vars)
+            #discriminator_optimizer = tf.train.AdadeltaOptimizer(0.001).minimize(discriminator_loss, var_list = discriminator_vars)
 
         sess.run(tf.global_variables_initializer())
 
